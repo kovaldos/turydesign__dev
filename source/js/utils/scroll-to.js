b238fa73cb6ad135c506237ff9d9078b34b443ff
kovaldos <ya.kovalev@ya-kovalev.ru>
2022-11-24 11:16:43 +0300

    feat: add footer

---
+++ source/js/utils/scroll-to.js
@@ -0,0 +1,36 @@
+
+/*   --------------------------------------------------------------*/
+/*  Функция для прокрутки с контролем скорости
+/*  --------------------------------------------------------------*/
+export function scrollTo(to, duration = 700) {
+  const
+    element = document.scrollingElement || document.documentElement;
+  const start = element.scrollTop;
+  const change = to - start;
+  const startDate = +new Date();
+  // t = current time
+  // b = start value
+  // c = change in value
+  // d = duration
+  const easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) {
+      return c / 2 * t * t + b;
+    }
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  };
+  const animateScroll = function () {
+    const currentDate = +new Date();
+    const currentTime = currentDate - startDate;
+    element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration), 10);
+    if (currentTime < duration) {
+      requestAnimationFrame(animateScroll);
+    } else {
+      element.scrollTop = to;
+    }
+  };
+  animateScroll();
+}
+
+// Кнопка скролла наверх - конец -
